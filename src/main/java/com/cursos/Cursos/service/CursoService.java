3e88b02fe9ccfc81d88f0dac47c64fd075030e08
Naomi <villarroel.na@gmail.com>
2025-05-20 20:32:17 -0400

    Se agrega el service

--- src/main/java/com/cursos/Cursos/service/CursoService.java
+++ src/main/java/com/cursos/Cursos/service/CursoService.java
@@ -1,4 +1,34 @@
 package com.cursos.Cursos.service;
 
+
+import com.cursos.Cursos.model.Curso;
+import com.cursos.Cursos.repository.CursoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class CursoService {
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> findAll() {
+        return cursoRepository.findAll();
+    }
+
+    public Curso findById(long id) {
+        return cursoRepository.findById(id).get();
+    }
+
+    public Curso save(Curso curso) {
+        return cursoRepository.save(curso);
+    }
+
+    public void delete(Long id) {
+        cursoRepository.deleteById(id);
+    }
 }
+

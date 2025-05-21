c3f7e7f5cf144dba2834b99b7be9be05807f7653
franalarana <fr.guerreroo@duocuc.cl>
2025-05-20 20:06:02 -0400

    Agregar modelo curso

--- src/main/java/com/cursos/Cursos/model/Curso.java
+++ src/main/java/com/cursos/Cursos/model/Curso.java
@@ -1,4 +1,37 @@
 package com.cursos.Cursos.model;
 
-public class Curso {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "curso")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+
+
+public class Curso
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String profesor;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String formato;
+
+
 }

d1cf036ed82031fb6c2aa6e09bda061ca7f624cd
franalarana <fr.guerreroo@duocuc.cl>
2025-06-08 19:40:11 -0400

    Creo el microservicio de usuarios

---
+++ src/main/java/com/usuarios/usuarios/controller/UsuarioController.java
@@ -0,0 +1,84 @@
+package com.usuarios.usuarios.controller;
+
+import org.apache.coyote.Response;
+import com.usuarios.usuarios.model.Usuario;
+import com.usuarios.usuarios.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
+public class UsuarioController
+{
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listar()
+    {
+        List<Usuario> usuarios = usuarioService.findAll();
+        if (usuarios.isEmpty())
+        {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> guardar(@RequestBody Usuario usuario)
+    {
+        Usuario usuarioNuevo = usuarioService.save(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioNuevo);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscar(@PathVariable Integer id)
+    {
+        try
+        {
+            Usuario usuario = usuarioService.findById(id);
+            return ResponseEntity.ok(usuario);
+        }
+        catch ( Exception e )
+        {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> actualizar(@PathVariable Integer id, @RequestBody Usuario usuario)
+    {
+        try
+        {
+            Usuario usuario1 = usuarioService.findById(id);
+            usuario1.setId(id);
+            usuario1.setNombreUsuario(usuario.getNombreUsuario());
+            usuario1.setContrasenia(usuario.getContrasenia());
+            usuarioService.save(usuario1);
+            return ResponseEntity.ok(usuario);
+
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id)
+    {
+        try
+        {
+            usuarioService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch ( Exception e)
+        {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/usuarios/usuarios/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package com.usuarios.usuarios.service;
+
+import com.usuarios.usuarios.model.Usuario;
+import com.usuarios.usuarios.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+
+public class UsuarioService
+
+
+{
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll()
+    {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(long id)
+    {
+        return usuarioRepository.findById(id).get();
+    }
+
+    public Usuario save(Usuario usuario)
+    {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void delete(Long id)
+    {
+        usuarioRepository.deleteById(id);
+    }
+
+
+}

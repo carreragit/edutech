fa4edfc823b5507b5e539e2966ef57ce5aa05bc6
demondwar <alonsodiego44@gmail.com>
2025-06-14 15:20:00 -0400

    API-soporte-auth

---
+++ base-microservicios/auth/src/main/java/com/auth/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.auth.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API para microservicio de autenticacion ")
+                        .version("1.0")
+                        .description("Documentacion API para microservicio de autenticacion"));
+    }
+}
--- base-microservicios/auth/src/main/java/com/auth/controller/AuthController.java
+++ base-microservicios/auth/src/main/java/com/auth/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.auth.controller;
 
 import com.auth.Dto.LoginRequest;
 import com.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-
+@Tag(name = "Autenticación", description = "API para manejar la autenticación de usuarios")
 public class AuthController {
     @Autowired
     private AuthService authService;
-
+    @Operation(summary = "Iniciar sesión", description = "Autentica las credenciales del usuario y devuelve un resultado")
     @PostMapping("/login")
     public ResponseEntity<String> login (@RequestBody LoginRequest request) {
         boolean resultado = authService.login(request );
